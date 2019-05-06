2c777ee0db4b8525c4ef46e69503ef07280c180b
Igli Grykshi <igli-grykshi@hotmail.com>
2019-05-06 17:22:17 +0200

    setup

---
+++ public/precache-manifest.3f94d1b897c058c8ec36afc3a5dc4875.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be83136f776ccffb84c6ec6336c031a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b446a3e754133bd5df10",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "7da735531a75da01a41b",
+    "url": "/static/js/2.1b4a64b9.chunk.js"
+  },
+  {
+    "revision": "b446a3e754133bd5df10",
+    "url": "/static/js/main.294f3421.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
